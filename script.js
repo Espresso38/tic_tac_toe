bd77f3cf59e313e2c758743ee20fef03af73ff59
Espresso38 <naj.tomalak@gmail.com>
2024-08-20 19:24:01 +0200

    Creating visual site of app and making it accesable

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const navbar = document.getElementById('nav');
+const container = document.getElementById('container');
+const winner = document.getElementById('winner');
+const change_1 = document.getElementById('change_1');
+const change_2 = document.getElementById('change_2');
+const reset = document.getElementById('reset');
+
 const GameBoard = {
     board: [
         ['','',''],
@@ -8,17 +15,36 @@ const GameBoard = {
     player_2: {player_name: "O", player_sign: "O"},
     player_turn: null,
 
+    createGameBoard: function() {
+        container.innerHTML = '';
+
+        for (let i = 0; i < 9; i++) {
+            const div = document.createElement('div');
+            div.classList.add('field');
+            div.innerText = this.board[Math.floor(i / 3)][i % 3];
+            div.setAttribute('data-index', i);
+            container.appendChild(div);
+        
+            div.addEventListener('click', () => {
+                let index = div.getAttribute('data-index');
+                this.makeMove(Math.floor(index / 3), index % 3);
+            });
+        }
+    },
+
     init: function() {
         this.player_turn = this.player_1;
+        this.createGameBoard();
     },
 
     resetGame: function() {
         this.board = [
             ['','',''],
             ['','',''],
-        ['','','']
+            ['','','']
         ];
         this.player_turn = this.player_1;
+        this.createGameBoard();
     },
 
     changePlayerName_1: function(name) {
@@ -58,12 +84,26 @@ const GameBoard = {
     makeMove: function(row, col) {
         if (this.board[row][col] === '') {
             this.board[row][col] = this.player_turn.player_sign;
+            this.createGameBoard();
+
             if (this.checkWin()) {
-                console.log(`Player ${this.player_turn.player_name} wins!`);
-                this.resetGame();
+                winner.innerText = `
+                Player ${this.player_turn.player_name} wins!
+                `;
+                winner.style.visibility = 'visible';
+                setTimeout(() => {
+                    winner.style.visibility = 'hidden';
+                    winner.innerText = '';
+                    this.resetGame();
+                }, 3000);
             } else if (this.checkDraw()) {
-                console.log('Draw!');
-                this.resetGame();
+                winner.innerText = 'Draw!';
+                winner.style.visibility = 'visible';
+                setTimeout(() => {
+                    winner.style.visibility = 'hidden';
+                    winner.innerText = '';
+                    this.resetGame();
+                }, 3000);
             } else {
                 this.player_turn = this.player_turn === this.player_1 ? this.player_2 : this.player_1;
             }
@@ -73,11 +113,18 @@ const GameBoard = {
     }
 };
 
-GameBoard.init();
+change_1.addEventListener('click', () => {
+    let name = prompt('Change Player 1 Name');
+    GameBoard.changePlayerName_1(name);
+});
 
-GameBoard.makeMove(0, 0);
-GameBoard.makeMove(2, 2);
-GameBoard.makeMove(0, 1);
-GameBoard.makeMove(2, 1);
-GameBoard.makeMove(1, 1);
-GameBoard.makeMove(2, 0);
+change_2.addEventListener('click', () => {
+    let name = prompt('Change Player 2 Name');
+    GameBoard.changePlayerName_2(name);
+});
+
+reset.addEventListener('click', () => {
+    GameBoard.resetGame();
+});
+
+GameBoard.init();
